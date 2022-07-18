873becf6f9d6ace0847ae858d31dc3fc4e818a9a
temitopeAdeyemo <95665063+temitopeAdeyemo@users.noreply.github.com>
2022-07-18 21:26:37 +0100

    Update lannister.controller.js

--- lannister.controller.js
+++ lannister.controller.js
@@ -1,52 +1,15 @@
 const LannisterUser = require("./lannister.model");
-const uuid = require("uuid");
-// console.log(uuid)
-// console.log(LannisterUser);
-
 const share = async (req, res, next) => {
   try {
-    const { amount, customerEmail, splitInfo, splitType, splitValue } =
+    const { amount, customerEmail, splitInfos, splitType, splitValue } =
       req.body;
-
-    // const payLoad = {
-    //   id: "1308",
-    //   amount: 12580,
-    //   currency: "NGN",
-    //   customerEmail: " hgvdchgdhgs@gmail.com",
-    //   splitInfo: [
-    //     {
-    //       splitType: "PERCENTAGE",
-    //       splitValue: 7,
-    //       splitEntityId: "LAN4422",
-    //     },
-    //     {
-    //       splitType: "FLAT",
-    //       splitValue: 450,
-    //       splitEntityId: "LAN4422",
-    //     },
-    //     {
-    //       splitType: "PERCENTAGE",
-    //       splitValue: 5,
-    //       splitEntityId: "LAN4422",
-    //     },
-    //     {
-    //       splitType: "RATIO",
-    //       splitValue: 4,
-    //       splitEntityId: "LAN4422",
-    //     },
-    //     {
-    //       splitType: "RATIO",
-    //       splitValue: 3,
-    //       splitEntityId: "LAN4422",
-    //     },
-    //   ],
-    // };
-    // const splitt = payload.splitInfo;
-    splitInfo.forEach((info) => {
-      const flatArray = [];
-      const percentageArray = [];
-      const ratioArray = [];
-      let availableBalance;
+    // const splitInfoo = [];
+    let Lannistercode = 2014;
+    const flatArray = [];
+    const percentageArray = [];
+    const ratioArray = [];
+    let availableBalance = amount;
+    splitInfos.forEach((info) => {
       if (info.splitType == "FLAT") {
         flatArray.push(info);
       } else if (info.splitType == "PERCENTAGE") {
@@ -55,20 +18,83 @@ const share = async (req, res, next) => {
         ratioArray.push(info);
       }
     });
+    const returnInfo = {
+      ID: "",
+      Balance: "",
+      splitBreakdown: [],
+    };
+    const resultArray = [];
+    flatArray.forEach((flat) => {
+      const flatObject = {};
+      flatAmount = flat.splitValue;
+      availableBalance = availableBalance - flatAmount;
+      Lannistercode = Lannistercode + 1;
+      flatObject["splitAmount"] = flatAmount;
+      flatObject["splitType"] = flat.splitType;
+      flatObject["splitValue"] = flat.splitValue;
+      flatObject["splitEntityId"] = `LNPYACC${Lannistercode}`;
+      let returnResult = {
+        splitEntityId: flatObject["splitEntityId"],
+        amount: flatObject["splitAmount"],
+      };
+      returnInfo.splitBreakdown.push(returnResult);
+      resultArray.push(flatObject);
+    });
+
+    percentageArray.forEach((percentage) => {
+      percentageAmount = (percentage.splitValue / 100) * availableBalance;
+      availableBalance = availableBalance - percentageAmount;
+      Lannistercode = Lannistercode + 1;
+      const percentageObject = {};
+      percentageObject["splitAmount"] = percentageAmount;
+      percentageObject["splitType"] = percentage.splitType;
+      percentageObject["splitValue"] = percentage.splitValue;
+      percentageObject["splitEntityId"] = `LNPYACC${Lannistercode}`;
+      let returnResult = {
+        splitEntityId: percentageObject["splitEntityId"],
+        amount: percentageObject["splitAmount"],
+      };
+      returnInfo.splitBreakdown.push(returnResult);
+      resultArray.push(percentageObject);
+    });
+    
+    let ratioSum = 0;
+    const ratioBalance = availableBalance;
+    ratioArray.forEach((ratio) => {
+      ratioSum = ratioSum + ratio.splitValue;
+    });
+    ratioArray.forEach((ratio) => {
+      ratioAmount = (ratio.splitValue / ratioSum) * ratioBalance;
+      availableBalance = availableBalance - ratioAmount;
+      Lannistercode = Lannistercode + 1;
+      const ratioObject = {};
+      ratioObject["splitAmount"] = ratioAmount;
+      ratioObject["splitType"] = ratio.splitType;
+      ratioObject["splitValue"] = ratio.splitValue;
+      ratioObject["splitEntityId"] = `LNPYACC${Lannistercode}`;
+      let returnResult = {
+        splitEntityId: ratioObject["splitEntityId"],
+        amount: ratioObject["splitAmount"],
+      };
+      returnInfo.splitBreakdown.push(returnResult);
+      resultArray.push(ratioObject);
+    });
+
     const newShare = new LannisterUser({
       amount,
       customerEmail,
-      splitInfo: [
-        {
-          splitType,
-          splitValue,
-          splitEntityId: uuid.v4(),
-        },
-      ],
+      splitInfo: resultArray,
+      balance: availableBalance,
     });
+
     await newShare.save();
+
+    returnInfo["ID"] = newShare._id;
+    returnInfo["Balance"] = newShare.balance;
+
     return res.status(200).json({
-      newShare,
+      // newShare,
+      returnInfo,
     });
   } catch (error) {
     console.log(error);
@@ -76,4 +102,4 @@ const share = async (req, res, next) => {
   }
 };
 
-module.exports = {share}
+module.exports = { share };
