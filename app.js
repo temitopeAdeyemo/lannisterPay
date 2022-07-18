d4cc76db63206a2e4e6387a16e109394aaaed706
temitopeAdeyemo <95665063+temitopeAdeyemo@users.noreply.github.com>
2022-07-18 21:27:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const shareRouter = require("./lannister.route")
-const PORT = 8080;
+const shareRouter = require("./lannister.route");
+const PORT = 8090;
 const dotenv = require("dotenv").config();
 const app = express();
 app.use(express.json());
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://compactpay:12345@compactpay.ldydh.mongodb.net/lannisterpay?retryWrites=true&w=majority",
+      process.env.MONGO_URI,
       {
         useUnifiedTopology: true,
         useNewUrlParser: true,
@@ -20,7 +20,7 @@ const connectDB = async () => {
   }
 };
 connectDB();
-app.use("/split-payments", shareRouter)
+app.use("/split-payments", shareRouter);
 app.get("/", (res) => {
   res.send({
     message: "Welcome to Lannister.",
